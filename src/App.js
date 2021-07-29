9e5c1f9308bf4d39f00664a322b48518fcff3717
Chris <spanosc1@tcnj.edu>
2021-07-29 17:55:27 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Header from './components/Header';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Header/>
+      </div>
+    );
+  }
+}
+
+export default App;
